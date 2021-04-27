d991aa8db360a0a1532afffbdfe79d6786430481
shehan82 <shehansandeepa82@gmail.com>
2021-04-27 20:46:02 +0530

    second page added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import CakeIcon from "@material-ui/icons/Cake";
+import Item from "./Item";
 function App() {
   return (
     <div className="app">
@@ -16,8 +17,8 @@ function App() {
         <div className="app_main_inner">
           <div className="app_main_innerText">
             <div className="app_main_innerText_top">
-              <div className="app_main_innerText_top1">GREATEST</div>
-              <div className="app_main_innerText_top2">CAKE FOR YOU ALL</div>
+              <div className="app_main_innerText_top1">SWEETEST</div>
+              <div className="app_main_innerText_top2">CAKE FOR YOU </div>
             </div>
             <div className="app_main_innerText_low">
               <span>READY IN 60 SECONDS .</span>
@@ -26,7 +27,29 @@ function App() {
           <div className="app_main_inner_button">place order</div>
         </div>
       </div>
-      <div className="app_next">SWEET CAKE</div>
+      <div className="app_second">
+        <div className="app_second_header">Choose your favourite</div>
+        <div className="app_second_itemBox">
+          <Item
+            imgUrl="https://i.ibb.co/9G6VGpH/pexels-pranjall-kumar-7381533.jpg"
+            name="Sweet Mingle"
+            desc="The very sweet Italian cake"
+            price="$ 19"
+          />
+          <Item
+            imgUrl="https://i.ibb.co/VT7K0TD/pexels-nataliya-vaitkevich-4692169.jpg"
+            name="Sweet Mingle"
+            desc="The very sweet Italian cake"
+            price="$ 19"
+          />
+          <Item
+            imgUrl="https://i.ibb.co/89XvvvK/pexels-irwan-fauzi-4858510.jpg"
+            name="Sweet Mingle"
+            desc="The very sweet Italian cake"
+            price="$ 19"
+          />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Item.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { CardMedia } from "@material-ui/core";
+import "./App.css";
+import img from "./assets/images/cake.jpg";
+
+function Item(props) {
+  return (
+    <div className="item_container">
+      <div className="image_container">
+        <img src={props.imgUrl} />
+      </div>
+      <div className="details_container">
+        <div className="details_container_name">{props.name}</div>
+        <div className="details_container_desc">
+          {'"  ' + props.desc + '  "'}
+        </div>
+        <div className="details_container_price">{props.price}</div>
+      </div>
+      <div className="button_container">Add to Cart</div>
+    </div>
+  );
+}
+
+export default Item;
