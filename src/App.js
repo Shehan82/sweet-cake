26347372c99e9b3a19fe8c43d4ce13ad330da462
shehan82 <shehansandeepa82@gmail.com>
2021-04-27 13:19:53 +0530

    make the nav header wingle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
-
+import CakeIcon from "@material-ui/icons/Cake";
 function App() {
   return (
     <div className="app">
-      <div className="app_main"></div>
-      <div className="app_next">hellooooo</div>
+      <div className="app_main">
+        <div className="app_navBar">
+          <div className="app_navBar_header">SWEET CAKE</div>
+          <div className="app_navBar_menu">
+            <div className="app_navBar_menu_text">Menu</div>
+            <div className="app_navBar_menu_icon">
+              <CakeIcon style={{ fontSize: 40 }} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="app_next">SWEET CAKE</div>
     </div>
   );
 }
