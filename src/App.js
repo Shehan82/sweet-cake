7f43cad751500f37dd8d07ffa990c43d13f28fcb
shehan82 <shehansandeepa82@gmail.com>
2021-04-27 16:13:15 +0530

    main page done

--- src/App.js
+++ src/App.js
@@ -5,13 +5,25 @@ function App() {
     <div className="app">
       <div className="app_main">
         <div className="app_navBar">
-          <div className="app_navBar_header">SWEET CAKE</div>
+          <div className="app_navBar_header">SWEET CAKE </div>
           <div className="app_navBar_menu">
             <div className="app_navBar_menu_text">Menu</div>
             <div className="app_navBar_menu_icon">
-              <CakeIcon style={{ fontSize: 40 }} />
+              <CakeIcon style={{ fontSize: 30 }} />
             </div>
           </div>
+        </div>
+        <div className="app_main_inner">
+          <div className="app_main_innerText">
+            <div className="app_main_innerText_top">
+              <div className="app_main_innerText_top1">GREATEST</div>
+              <div className="app_main_innerText_top2">CAKE FOR YOU ALL</div>
+            </div>
+            <div className="app_main_innerText_low">
+              <span>READY IN 60 SECONDS .</span>
+            </div>
+          </div>
+          <div className="app_main_inner_button">place order</div>
         </div>
       </div>
       <div className="app_next">SWEET CAKE</div>
